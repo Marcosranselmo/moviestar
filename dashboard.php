880a034a184d8cd08c8258872a4bcc3f2271453b
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-04-05 16:33:49 -0300

    alinhando-itens-button

--- dashboard.php
+++ dashboard.php
@@ -18,7 +18,7 @@
   <div id="main-container" class="container-fluid">
     <h2 class="section-title">Dashboard</h2>
     <p class="section-description">Adicione ou atualize as informações dos filmes que você enviou</p>
-    <div class="col-md-12" id="add-movie-container">
+    <div class="col-md-12 mb-3" id="add-movie-container">
       <a href="<?= $BASE_URL ?>newmovie.php" class="btn card-btn">
         <i class="fas fa-plus"></i> Adicionar Filme
       </a>
