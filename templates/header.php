6c6340ac681e477b429e076a745c61bd90b7ade7
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-04-06 20:53:41 -0300

    alinhando-logo

--- templates/header.php
+++ templates/header.php
@@ -38,7 +38,7 @@
     <nav id="main-navbar" class="navbar navbar-expand-lg">
       <a href="<?= $BASE_URL ?>" class="navbar-brand">
         <img src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar" id="logo">
-        <span id="moviestar-title">MovieStar</span>
+        <span class="mb-2 ml-3" id="moviestar-title">MovieStar</span>
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars"></i>
