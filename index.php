20aadbbc8dbe67fe621de85e5ecdb9c7f547f442
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-04-26 17:03:26 -0300

    adaptando-carousel-part-3

--- index.php
+++ index.php
@@ -17,16 +17,22 @@
 ?>
   <div id="main-container" class="container-fluid">
     
-    <h2 class="section-title">Filmes novos</h2>
-    <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieStar</p>
-    <div class="movies-container">
-      <?php foreach($latestMovies as $movie): ?>
-        <?php require("templates/movie_card.php"); ?>
-      <?php endforeach; ?>
-      <?php if(count($latestMovies) === 0): ?>
-        <p class="empty-list">Ainda não há filmes cadastrados!</p>
-      <?php endif; ?>
-    </div>
+
+      <h2 class="section-title">Filmes novos</h2>
+      <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieStar</p>
+      <iframe id='slide'>
+      <div class="movies-container">
+
+          <?php foreach($latestMovies as $movie): ?>
+            <?php require("templates/movie_card.php"); ?>
+          <?php endforeach; ?>
+          <?php if(count($latestMovies) === 0): ?>
+            <p class="empty-list">Ainda não há filmes cadastrados!</p>
+          <?php endif; ?>
+        
+      </div>
+      </iframe>
+
 
     <h2 class="section-title">Ação</h2>
     <p class="section-description">Veja os melhores filmes de ação</p>
--- templates/movie_card.php
+++ templates/movie_card.php
@@ -5,17 +5,83 @@
   }
 
 ?>
-<div class="card movie-card">
-  <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>img/movies/<?= $movie->image ?>')"></div>
-  <div class="card-body">
-    <p class="card-rating">
-      <i class="fas fa-star"></i>
-      <span class="rating"><?= $movie->rating ?></span>
-    </p>
-    <h5 class="card-title">
-      <a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>"><?= $movie->title ?></a>
-    </h5>
-    <a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>" class="btn btn-primary rate-btn">Avaliar</a>
-    <a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>" class="btn btn-primary card-btn">Conhecer</a>
-  </div>
-</div>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <!-- head -->
+    <meta charset="utf-8 ">
+    <meta name="viewport " content="width=device-width, initial-scale=1.0 " />
+
+    <title>
+        Responsive Carousel </title>
+
+    <!-- Stylesheets -->
+
+    <link rel="stylesheet " href="styles/style.css ">
+
+
+    <link rel="stylesheet " href="styles/carousel.min.css ">
+
+
+    <!-- javascript -->
+    <script src="scripts/jquery-3.5.1.min.js "></script>
+    <script src="scripts/carousel.js "></script>
+</head>
+
+<body>
+
+    <!--  Demos -->
+    <section id="slaide">
+        <div class="row">
+
+
+          <div class="card movie-card">
+            <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>img/movies/<?= $movie->image ?>')"></div>
+            <div class="card-body">
+              <p class="card-rating">
+                <i class="fas fa-star"></i>
+                <span class="rating"><?= $movie->rating ?></span>
+              </p>
+              <h5 class="card-title">
+                <a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>"><?= $movie->title ?></a>
+              </h5>
+              <a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>" class="btn btn-primary rate-btn">Avaliar</a>
+              <a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>" class="btn btn-primary card-btn">Conhecer</a>
+            </div>
+          </div>
+
+          <script>
+                    $(document).ready(function() {
+                        $('.car-carousel').carCarousel({
+                            loop: false,
+                            margin: 5,
+                            responsiveClass: true,
+                            responsive: {
+                                0: {
+                                    items: 2,
+                                    nav: true
+                                },
+                                600: {
+                                    items: 4,
+                                    nav: true
+                                },
+                                1000: {
+                                    items: 7,
+                                    nav: true,
+                                    loop: false,
+                                    margin: 3
+                                }
+                            }
+                        })
+                    })
+                </script>
+        </div>
+    </section>
+
+</body>
+
+</html>
